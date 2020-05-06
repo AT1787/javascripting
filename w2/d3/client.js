f39d7740b2fef962779ec858816d930893558649
Andrew Trac <andrewtrac87@hotmail.com>
2020-05-06 17:36:33 +0000

    added a move function and a setTimer

--- w2/d3/client.js
+++ w2/d3/client.js
@@ -8,9 +8,19 @@ const connect = function() {
 
     conn.setEncoding('utf8'); 
 
-    conn.on('connect', (name) => {
+    conn.on('connect', () => {
         console.log("Successfully connected to game server")
+    
+        /*
+        for (let i = 0; i < 10000; i = i + 1000)
+        setTimeout(() => {
+            conn.write("Move: up")
+            
+        }, i) */
+        
+        
         conn.write("Name: AND")
+       
       })  
   
     conn.on('data', (data) => {
