260910c15e5e527671b060ee283d2b24c69f9590
Andrew Trac <andrewtrac87@hotmail.com>
2020-05-06 18:03:26 +0000

    created new module and moved setupInput in to input.js

--- w2/d3/play.js
+++ w2/d3/play.js
@@ -1,23 +1,6 @@
 const { connect }  = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  handleUserInput()
-  return stdin;
-}
-
-const handleUserInput = () => {
-  process.stdin.on('data', (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-});
-}
-
 setupInput();
 
